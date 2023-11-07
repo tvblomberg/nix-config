67fd657019353464bb7921e3341f3804dca611f1
Thomas V. Blomberg <thomas.v.blomberg@gmail.com>
2023-11-06 18:43:15 -0600

    Add home-manager

--- configuration.nix
+++ configuration.nix
@@ -66,6 +66,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    home-manager 
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #   wget
   ];
