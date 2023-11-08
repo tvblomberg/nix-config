b1057f5ac0d8b2c125618e5f34cfa9593fdb3e3a
Thomas V. Blomberg <thomas.v.blomberg@gmail.com>
2023-11-07 21:32:22 -0600

    Working on adding the new laptop

--- flake.nix
+++ flake.nix
@@ -9,9 +9,11 @@
       url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    hyprland.url = "github:hyprwm/Hyprland";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }:
+  outputs = { self, nixpkgs, home-manager, hyprland, ... }:
     let
       system = "x86_64-linux";
       lib = nixpkgs.lib;
@@ -24,13 +26,23 @@
 	  ./configuration-vm.nix
 	];
       };
+      hal = lib.nixosSystem {
+        inherit system;
+	modules = [
+	  ./configuration-hal.nix
+	];
+      };
     };
     homeConfigurations."invincent" = home-manager.lib.homeManagerConfiguration {
       inherit pkgs;
 
      # Specify your home configuration modules here, for example,
      # the path to your home.nix.
-      modules = [ ./home.nix ];
+      modules = [ 
+        ./home.nix
+        hyprland.homeManagerModules.default
+        { wayland.windowManager.hyprland.enable = true; } 
+      ];
 
      # Optionally use extraSpecialArgs
      # to pass through arguments to home.nix
